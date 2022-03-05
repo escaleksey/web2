f4e1a066a377f42840448be49dc27de07a5d313a
Igor_Baryshnikov <jak9rrr@mail.ru>
2022-03-05 16:23:33 +0500

    new task3

--- index.py
+++ index.py
@@ -7,28 +7,40 @@ from data.jobs import Jobs
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
+def user_create(surname, name, age, position, speciality, address, email, hashed_password):
+    user = User()
+    user.surname = surname
+    user.name = name
+    user.age = age
+    user.position = position
+    user.speciality = speciality
+    user.address = address
+    user.email = email
+    user.hashed_password = hashed_password
+    return user
+
 
 def main():
     db_session.global_init("db/mars.db")
     db_sess = db_session.create_session()
-    user = User()
-    user.surname = "Scott"
-    user.name = "Ridley"
-    user.age = 21
-    user.position = "captain"
-    user.speciality = "research engineer"
-    user.address = "module_1"
-    user.email = "scott_chief@mars.org"
-    user.hashed_password = "cap"
+    db_sess.add(user_create("Scott", "Ridley", 21, "captain",
+                            "research engineer", "module_1", "scott_chief@mars.org", "cap"))
+    db_sess.commit()
+    db_sess.add(user_create("Kushnarev", "Aleksey", 16, "student",
+                            "programmer", "module_2", "Aleksey@mars.org", "notcap"))
+    db_sess.commit()
+    db_sess.add(user_create("Popov", "Arthur", 17, "DJ",
+                            "dj", "module_1", "Arthur@mars.org", "dj"))
     job = Jobs()
     job.team_leader = 1
     job.job = 'deployment of residential modules 1 and 2'
     job.work_size = 15
     job.collaborators = '2, 3'
     job.is_finished = False
-    db_sess.add(user)
+
     db_sess.add(job)
     db_sess.commit()
+    db_sess.close()
     #app.run()
 
 
